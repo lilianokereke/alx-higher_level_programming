eef0b12ddb7c5335d3d71059f6a2ce3506843318
lilianokereke <okerekeezinne2003@gmail.com>
2022-09-21 14:53:59 -0700

    combo1

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for n in range(1, 10):
         if i == 8 and n == 9:
-            print("{}{}".format(i, n), end="")
+            print("{}{}, ".format(i, n), end="")
         else:
-            print("{}{}".format(i, n), end="")
+            print("{}{}, ".format(i, n), end="")
