6a31e425cc3b78c1b27f0228dede43646cb7c3aa
lilianokereke <okerekeezinne2003@gmail.com>
2022-09-22 05:40:49 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin//python3
-from add_0 import add
+import add_0
 
 a = 1
 b = 2
-print("{a} + {b} =")
+
+print(f"{a} + {b} =")
 add(a, b)
--- 0x02-python-import_modules/00.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def add(a, b):
-    a = 1
-    b = 2
-    print(f"{a} + {b} = ", end="")
-    return a + b
-
-
-print(add(1, 2))
