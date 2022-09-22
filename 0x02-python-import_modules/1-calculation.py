517461962c416ce761e93d80c0f002f048f31ab2
lilianokereke <okerekeezinne2003@gmail.com>
2022-09-22 09:23:53 -0700

    caulations

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     b = 5
 
 
-    print(a, " + ", b, "=", add(a,b))
-    print(a, " - ", b, "=", sub(a,b))
-    print(a, " * ", b, "=", mul(a,b))
-    print(a, " / ", b, "=", int(div(a,b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, int(div(a, b))))
