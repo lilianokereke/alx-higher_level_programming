d37da0af6004f97bda54cbfde515f16c3e812171
lilianokereke <okerekeezinne2003@gmail.com>
2022-09-22 05:30:57 -0700

    addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python3
-def add(a, b):
-    a = 1
-    b = 2
-    print(f"{a} + {b} = ", end="")
-    return a + b
+#!/usr/bin//python3
+from add_0 import add
 
-
-print(add(1, 2))
+a = 1
+b = 2
+print("{a} + {b} =")
+add(a, b)
