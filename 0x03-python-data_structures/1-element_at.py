83ad11df0206769ed588b3f7efdd140f4fea8cad
lilianokereke <okerekeezinne2003@gmail.com>
2022-09-24 02:47:55 -0700

    index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    elem = " "
     if idx < 0:
         return ("None")
     elif idx > len(my_list):
         return ("None")
     else:
-        elem += ("{}".format(my_list[idx]))
-        return elem
+        return my_list[idx]
