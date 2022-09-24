1d8c1036c6654b7c27e24296522ed856cf574dbc
lilianokereke <okerekeezinne2003@gmail.com>
2022-09-24 03:46:37 -0700

    remove

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_str = ""
+    if my_string == (""):
+        return
     for i in my_string:
         if i == 'c' or i == 'C':
             continue
-        new_str+= i
+        new_str += i
     return new_str
-
-p = "lili ic Coming"
-print(no_c(p))
-print(no_c("abcd"))
-print(no_c("12cvy iklcm i"))
