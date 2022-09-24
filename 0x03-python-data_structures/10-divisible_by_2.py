03a2217faa190cec3b01a377b7124c38d8568505
lilianokereke <okerekeezinne2003@gmail.com>
2022-09-24 08:57:36 -0700

    true or false

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    lit = []
+    for i in my_list:
+        if i % 2 == 0:
+            lit. append(True)
+        else:
+            lit.append(False)
+    return lit
