fd85f3a3ee3217f238e8661137a15ee6a86d904a
lilianokereke <okerekeezinne2003@gmail.com>
2022-09-26 05:32:17 -0700

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tup = (len(sentence), sentence[0])
-    return (tup)
+    if sentence == ():
+        return None
+    else:
+        tup = (len(sentence), sentence[0])
+        return (tup)
