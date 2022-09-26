cc8f80a22d81be0d937e35c81578cd2552663d07
lilianokereke <okerekeezinne2003@gmail.com>
2022-09-26 06:00:42 -0700

    unpacking

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == ():
-        return None
-    else:
-        tup = (len(sentence), sentence[0])
-        return (tup)
+    length = len(sentence)
+    first_char = sentence[0] if length > 0 else "None"
+    tup = length, first_char
+    return(tup)
+sentence = ()
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
+
