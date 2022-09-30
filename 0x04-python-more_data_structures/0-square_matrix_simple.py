e10e1c38fbd6eded92b8bc1b00a4c449f7a7ccbc
lilianokereke <okerekeezinne2003@gmail.com>
2022-09-30 14:05:56 -0700

    update dictionary

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
+    #new_matrix = []
+   # m  = [x * x for x in new_matrix]
+    m = lambda x: x**2
+    new = list(map(m, matrix))
+    return new
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
 
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sor = sorted(a_dictionary)
-    print (sor, get.values(sor))
-    return sor
-
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-print_sorted_dictionary(a_dictionary)
-
+    for keys in sorted(a_dictionary):
+        print("{}:{}".format(keys, a_dictionary[keys]))
