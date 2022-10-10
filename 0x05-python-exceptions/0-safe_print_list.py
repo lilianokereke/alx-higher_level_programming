e050386d2e1215880c46878063707eceecf4b0b0
lilianokereke <okerekeezinne2003@gmail.com>
2022-10-10 08:48:21 -0700

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
-            print(my_list[i], end="")
-    except (IndexError, TypeError):
-        pass
+    length = 0
+
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            length += 1
+        except IndexError:
+            pass
+    print()
+    return length
