3fb566ec01a5ff7df6e3aa0fb77e0466a6d869b0
lilianokereke <okerekeezinne2003@gmail.com>
2022-10-10 11:08:26 -0700

    just int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        if value is int(value):
-            print("{:d}".format(value))
-            return True
-    except ValueError:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
         return False
