bc883282f561f146305f6ac9cd7ab63f4578c6b0
lilianokereke <okerekeezinne2003@gmail.com>
2022-10-24 14:56:21 -0700

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""This module contains the `lookup()` function."""
+
+
+def lookup(obj):
+    """Returns all attributes of an object.
+            Args:
+             obj(Any): The object
+    """
+    if obj is None:
+        return []
+    return dir(obj)
