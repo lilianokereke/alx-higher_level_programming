afe21ee98d197a125685942cf9583b904368a587
lilianokereke <okerekeezinne2003@gmail.com>
2022-11-30 11:27:41 -0800

    delete database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
 -- create database and If the database hbtn_0c_0 already exists,it doesn't fail
-CREATE DATABASE IF NOT EXISTS 'hbtn_0c_0'
+CREATE DATABASE IF NOT EXISTS 'hbtn_0c_0';
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- Delete a database, if database doesn't exist, code won't fail
-DELETE DATABASE IF EXIST 'hbtn_0c_0'
+DELETE DATABASE IF EXIST 'hbtn_0c_0';
